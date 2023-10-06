0e15b7dd3317f547ead985882c441824276d2360
Shreyansh Shikhar Srivastava <133575578+shreyanshshikhar@users.noreply.github.com>
2023-10-07 00:55:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 let masterPlay = document.getElementById('masterPlay');
 let gif = document.getElementById('gif');
 let songIndex = 0;
+const currentTimeDisplay = document.querySelector('timestamp');
+
 let masterSongName = document.getElementById('masterSongName');
 let myProgressBar = document.getElementById('myProgressBar');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs = [{
         songName: "town",
-        filePath: "song1.mp3",
+        filePath: "songs/song1.mp3",
         coverPath: "https://i.pinimg.com/736x/d5/05/ee/d505ee867802323770ec4b02a9134b25.jpg"
     },
     {
         songName: "wohoo",
-        filePath: "song2.mp3",
+        filePath: "songs/song2.mp3",
         coverPath: "https://images.template.net/90560/music-album-cover-template-05xmk.jpeg"
     },
     {
         songName: "Beat",
-        filePath: "song3.mp3",
+        filePath: "songs/song3.mp3",
         coverPath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4CizKs6VxFXB6SZpF4dv27Gvk3RSqdyl4NoN99MUzg9kNtYeEnXKgYWf3A3hOUHY8XRU&usqp=CAU"
     },
     {
         songName: "trip",
-        filePath: "song4.mp3",
+        filePath: "songs/song4.mp3",
         coverPath: "https://marketplace.canva.com/EAFWz37wwl0/1/0/1600w/canva-black-minimalist-photocentric-rose-on-fire-hip-hop-album-cover-laJL2q01ZUU.jpg"
     }, {
         songName: "beap",
-        filePath: "song5.mp3",
+        filePath: "songs/song5.mp3",
         coverPath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMlo1SEctH8EtnneE5uAPCOsw_ZFmfDnLB6_7RwyhkUThz0t5ms-D5VEajd_n5imS1f2g&usqp=CAU"
     },
     // Add more song objects as needed
 ];
 
 let audioElement = new Audio(songs[0].filePath);
+const currentTime = audioElement.currentTime;
+const duration = audioElement.duration;
+const minutes = Math.floor(currentTime / 60);
+const seconds = Math.floor(currentTime % 60);
+const totalMinutes = Math.floor(duration / 60);
+const totalSeconds = Math.floor(duration % 60);
+
+
+
+function formatTime(timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
 
 
 //play pause logic 
@@ -53,17 +69,50 @@ masterPlay.addEventListener('click', () => {
 });
 
 
+
+
+
 songItems.forEach((element, i) => {
     element.querySelector("img").src = songs[i].coverPath;
     element.querySelector(".songName").innerText = songs[i].songName;
 });
 
+function updateTimestampDisplay() {
+    const currentTime = audioElement.currentTime;
+    const duration = audioElement.duration;
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = Math.floor(currentTime % 60);
+
+    // Loop through all the songItem elements and update their timestamp
+    songItems.forEach((element, index) => {
+        const timestampElement = element.querySelector(".timestamp");
+
+        if (index === songIndex) {
+            // Highlight the currently playing song's timestamp
+            timestampElement.style.color = "black";
+
+
+        } else {
+            timestampElement.style.color = "transparent";
+
+
+        }
+        timestampElement.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+    });
+}
+
 
 //range bar logic
 audioElement.addEventListener('timeupdate', () => {
+
     progress = parseInt((audioElement.currentTime / audioElement.duration
 
     ) * 100);
+
+
+    updateTimestampDisplay();
+
     myProgressBar.value = progress;
 });
 myProgressBar.addEventListener('change', () => {
@@ -71,14 +120,18 @@ myProgressBar.addEventListener('change', () => {
 });
 
 const makeAllPlays = () => {
-        Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
 
-            element.classList.remove('fa-pause-circle');
-            element.classList.add('fa-play-circle');
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
 
-        })
-    }
-    //small play button
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+
+    })
+
+}
+
+
+//small play button
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element, index) => {
     element.addEventListener('click', function(e) {
         // If the audio is playing, pause it; otherwise, play it
@@ -88,6 +141,7 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element, in
             e.target.classList.add('fa-play-circle');
             masterPlay.classList.remove('fa-pause-circle');
             masterPlay.classList.add('fa-play-circle');
+
             gif.style.opacity = 0; // Hide the audio beat GIF
         } else {
             makeAllPlays();
